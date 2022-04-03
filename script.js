9954697166fab1200d11290adc66354b6e6843bb
Befio Rangga <82318879+Befiorangga@users.noreply.github.com>
2022-04-03 15:23:59 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('menu');
-    const button = document.getElementById('btn1');
-
-      hamburger.addEventListener('click', () => {
-      navLinks.classList.toggle('show');
-      button.classList.toggle('show');
-    });
