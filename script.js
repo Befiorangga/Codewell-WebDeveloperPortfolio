96d85d968b6367a983edc4cd9d9b63b3ec09eee0
Befio Rangga <82318879+Befiorangga@users.noreply.github.com>
2022-04-03 15:16:58 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('menu');
+    const button = document.getElementById('btn1');
+
+      hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+      button.classList.toggle('show');
+    }); 
+  </script>
